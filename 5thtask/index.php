07b345b7cb6361bad545b230c260a5a7fae55779
Mirik1979 <lya-job@yandex.ru>
2018-11-24 18:26:33 +0300

    5th

---
+++ 5thtask/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link href="styles/site.css" rel="stylesheet">
+    <title>Онлайн магазин</title>
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="header">
+			</div>
+			<!--сделано выравнивание по флекс технологии 15.02.2018 -->
+			<!--убрана горизонтальная прокрутка 16.02.2018 -->
+			<div class="menu">
+				<span>Главная</span>
+				<span><a href="catalogue.html">Каталог</a></span>
+				<span><a href="contacts.html">Контакты</a></span>
+			</div>		 
+		</div>
+	</header>
+		<div class="center">
+			<h1>Интернет-магазин по продаже книг</h1>
+			<p>Добро пожаловать в наш Интернет-магазин, где вы найдете широкий выбор книг!</p>	
+			<img class="imagesize" src="images/Britanica.jpg" alt="Британика"/>
+			<img class="imagesize" src="images/art.jpg" alt="Искусство"/>	
+			<img class="imagesize" src="images/pushkin.png" alt="Книги Пушкина"/>
+			<img class="imagesize" src="images/zamech.png" alt="Жизнь замечательных людей"/>
+		</div>	
+	</div>	
+	<footer>
+	<div class="footer"> 	
+			<!--добавлены пиктограммы соцсетей 15.02.2018 -->
+			<!--пиктограмма по всему сайту прижата к низу окна браузера 16.02.2018 -->
+			<div class="social">
+				<a href="#"></a>
+				<a href="#"></a>
+				<a href="#"></a>
+				<a href="#"></a>			
+			</div>	
+			<div class="footer_text"><span>Все права защищены <sup>&copy;</sup></span></div>
+		</div>		
+</footer>
+</body>
+</html>
